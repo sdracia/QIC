34fbb08e8d2803e215cf640dc950cc7b542bd53e
sdracia <turci.andrea01@gmail.com>
2024-11-15 18:25:28 +0100

    more on sparse matrices

--- 04_Assignement/sparse_matrix.py
+++ 04_Assignement/sparse_matrix.py
@@ -3,11 +3,11 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import scipy.sparse.linalg as spl
-
+import seaborn as sns
 
 import functions as fu
 
-def sparse_matrix(L, omega, sizes, order):
+def sparse_matrix_one_eig(L, omega, sizes, order):
 
     num_sizes = len(sizes)
 
@@ -46,6 +46,56 @@ def sparse_matrix(L, omega, sizes, order):
     ax.set_title("Time to find the lowest eigenvalue", fontsize=16)
 
     plt.grid(True, linestyle='--', alpha=0.7)
+    plt.show()
+
+    return matrix, timings
+
+
+
+def sparse_matrix_heatmap(L, omega, sizes, order, num_eig):
+
+    num_sizes = len(sizes)
+
+    timings = np.zeros((num_eig, num_sizes))
+
+    for idx_1, size in enumerate(sizes):
+        deltax = (2*L)/size
+        x_i = np.array([(((2*L)/size)*i - L) for i in range(size)])
+
+        K = fu.kinetic_gen(size, deltax, order)
+        V = fu.potential_gen(size, x_i, omega)
+
+        A = K + V
+
+        #SPARSE MATRIX
+        matrix = sp.csc_matrix(A, shape=(size, size))
+
+        tic = time.time()
+        eigvl, eigvc = np.linalg.eigh(matrix.todense())
+        dense_timing = time.time() - tic
+
+        list_values = np.arange(1, num_eig+1)
+
+        for idx_2, n in enumerate(list_values):
+            tic = time.time()
+            eigvl, eigvc = spl.eigsh(matrix, k=n, which="SA")
+            sparse_timing = time.time() - tic
+            diff_timing = dense_timing - sparse_timing
+            timings[idx_2, idx_1] = diff_timing
+
+
+        
+    # Visualizzazione delle heatmaps
+    plt.figure(figsize=(12,8))
+
+    # Heatmap per gli errori sugli autovalori
+    sns.heatmap(timings,  annot=False, cmap="YlGnBu", 
+                xticklabels=sizes, yticklabels=np.arange(1, num_eig+1))
+    plt.title("Time Difference (Dense - Sparse) for Eigenvalue Calculation", fontsize=16)
+    plt.xlabel("Matrix Size", fontsize=16)
+    plt.ylabel("Number of Eigenvalues", fontsize=16)
+
+    plt.tight_layout()
     plt.show()
 
     return matrix, timings
