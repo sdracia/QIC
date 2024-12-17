74aa0207c1dc0f9b1cb54d9569126e2e7909cc44
sdracia <turci.andrea01@gmail.com>
2024-12-17 14:12:45 +0100

    functions fixed

--- 07_Assignement/sparse.py
+++ 07_Assignement/sparse.py
@@ -22,7 +22,6 @@ def pauli_matrices():
     s_z = csr_matrix([[1, 0], [0, -1]])
     return s_x, s_y, s_z
 
-# ===========================================================================================================
 
 def ising_hamiltonian(N, l):
     """
@@ -61,9 +60,6 @@ def ising_hamiltonian(N, l):
     H = l * H_nonint + H_int
     return H
 
-# ===========================================================================================================
-# EIGENVALUES
-# ===========================================================================================================
 
 def diagonalize_ising(N_values, l_values):
     """
@@ -99,6 +95,12 @@ def diagonalize_ising(N_values, l_values):
             eigenvectors[(N, l)] = eigvec
 
     return eigenvalues, eigenvectors
+
+
+
+# ===========================================================================================================
+# MAGNETIZATION
+# ===========================================================================================================
 
 
 def magnetization_z(N):
@@ -164,6 +166,12 @@ def compute_magnetization(N, l_vals):
     return magnetizations
 
 
+
+# ===========================================================================================================
+# DELTA E
+# ===========================================================================================================
+
+
 def compute_DeltaE(N, l_vals):
 
     DeltasE = []
@@ -172,16 +180,122 @@ def compute_DeltaE(N, l_vals):
         H = ising_hamiltonian(N, l)
 
         # Compute ground state
-        eigval, eigvec = eigsh(H, k=2, which='SA')
+        eigval, eigvec = eigsh(H, k=3, which='SA')
 
         first_eigval = eigval[0]
-        second_eigval = eigval[1]
+        second_eigval = eigval[2]
 
         delta = np.abs(first_eigval/N - second_eigval/N)
 
         DeltasE.append(delta)
 
     return DeltasE
+
+
+
+# ===========================================================================================================
+# ENTROPY
+# ===========================================================================================================
+
+
+
+def get_reduced_density_matrix(psi, loc_dim, n_sites, keep_indices,
+    print_rho=False):
+    """
+    Parameters
+    ----------
+    psi : ndarray
+        state of the Quantum Many-Body system
+    loc_dim : int
+        local dimension of each single site of the QMB system
+    n_sites : int
+        total number of sites in the QMB system
+    keep_indices (list of ints):
+        Indices of the lattice sites to keep.
+    print_rho : bool, optional
+        If True, it prints the obtained reduced density matrix], by default False
+
+    Returns
+    -------
+    ndarray
+        Reduced density matrix
+    """
+    if not isinstance(psi, np.ndarray):
+        raise TypeError(f'density_mat should be an ndarray, not a {type(psi)}')
+
+    if not np.isscalar(loc_dim) and not isinstance(loc_dim, int):
+        raise TypeError(f'loc_dim must be an SCALAR & INTEGER, not a {type(loc_dim)}')
+
+    if not np.isscalar(n_sites) and not isinstance(n_sites, int):
+        raise TypeError(f'n_sites must be an SCALAR & INTEGER, not a {type(n_sites)}')
+
+    # Ensure psi is reshaped into a tensor with one leg per lattice site
+    psi_tensor = psi.reshape(*[loc_dim for _ in range(int(n_sites))])
+    # Determine the environmental indices
+    all_indices = list(range(n_sites))
+    env_indices = [i for i in all_indices if i not in keep_indices]
+    new_order = keep_indices + env_indices
+    # Rearrange the tensor to group subsystem and environment indices
+    psi_tensor = np.transpose(psi_tensor, axes=new_order)
+    # print(f"Reordered psi_tensor shape: {psi_tensor.shape}")
+    # Determine the dimensions of the subsystem and environment for the bipartition
+    subsystem_dim = np.prod([loc_dim for i in keep_indices])
+    env_dim = np.prod([loc_dim for i in env_indices])
+    # Reshape the reordered tensor to separate subsystem from environment
+    psi_partitioned = psi_tensor.reshape((subsystem_dim, env_dim))
+    # Compute the reduced density matrix by tracing out the env-indices
+    RDM = np.tensordot(psi_partitioned, np.conjugate(psi_partitioned), axes=([1], [1]))
+    # Reshape rho to ensure it is a square matrix corresponding to the subsystem
+    RDM = RDM.reshape((subsystem_dim, subsystem_dim))
+
+    # PRINT RHO
+    if print_rho:
+        print('----------------------------------------------------')
+        print(f'DENSITY MATRIX TRACING SITES ({str(env_indices)})')
+        print('----------------------------------------------------')
+        print(RDM)
+
+    return RDM
+
+
+def entropy(N_values, l, loc_dim):
+
+    entropies = []
+
+    for i,N in enumerate(N_values):
+        N = int(N)
+        # Costruzione dell'Hamiltoniano
+        H = ising_hamiltonian(N, l)
+        # H = sp.ising_hamiltonian(N, l_critical[i])
+
+
+        # Calcolo del ground state
+        eigval, eigvec = eigsh(H, k=1, which='SA')
+        ground_state = eigvec[:, 0]
+
+        rho = np.outer(ground_state, np.conj(ground_state))
+
+
+        keep_indices = list(range(N // 2))  # Mantengo i primi N/2 spin
+        rhoA = get_reduced_density_matrix(ground_state, loc_dim, N, keep_indices)
+
+
+        # SVD per ottenere i valori singolari
+        U, Lambda, Vh = np.linalg.svd(rhoA, full_matrices=False)
+
+        # Calcolo dell'entropia di entanglement
+        entropy = -np.sum(Lambda**2 * np.log(Lambda**2))
+        entropies.append(entropy)
+
+
+
+    return entropies
+
+
+
+# ===========================================================================================================
+# PLOTTING AND VISUALIZATION
+# ===========================================================================================================
 
 
 def plot_eigenvalues(N_values, l_values, k):
