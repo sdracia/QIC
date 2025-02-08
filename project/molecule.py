7f4059a66b8cf2d01485a9e0ecd55e72d8bbf258
sdracia <turci.andrea01@gmail.com>
2025-02-08 19:08:25 +0100

    advancements

--- project/molecule.py
+++ project/molecule.py
@@ -29,8 +29,8 @@ class CaH:
     # gj_list: list[float] = [-1.35, -1.35, -1.35, -1.34, -1.34, -1.34, -1.34, -1.34, -1.33, -1.33, -1.33, -1.32, -1.32, -1.31, -1.31]
 
     # cij_list: list[float] = [8.27, 8.26, 8.26, 8.26, 8.26, 8.25, 8.25, 8.24, 8.24, 8.23, 8.22, 8.21, 8.20, 8.19, 8.18]
-
-    br_ghz: float = 144.0   #GHz
+    br_ghz: float = 142.5017779
+    # br_ghz: float = 144.0   #GHz
     """rotational constant, in Hz"""
     omega_0_thz: float = 750.0      # THz
     """frequency of the electronic transition between ground state and 1st excited state, in THz"""
@@ -166,7 +166,7 @@ class CaH:
             xi = False  # calculate xi = - states
             for m in self.m_csi_minus(j):
                 x = 1 / 2 * sqrt(cij**2 * ((j + 1 / 2) ** 2 - m**2) + (cij * m - self.cb_khz * (gj - gI)) ** 2)
-                y = -self.cb_khz / 2 * (gj - gI) + m * cij / 2
+                y = -self.cb_khz * (gj - gI) / 2  + m * cij / 2
                 if m == -j - 0.5:   # Extreme state. Edge left state
                     spin_up = 0.0
                     spin_down = 1.0
@@ -346,11 +346,21 @@ class CaH:
         colors = spin_up**2 - spin_down**2
 
         fig, ax = plt.subplots(figsize=(12, 8))
-        ax.scatter(m, energies, marker="_", c=colors, cmap="plasma", s=500, linewidths=5)
+        # ax.scatter(m, energies, marker="_", c=colors, cmap="plasma", s=500, linewidths=5)
+        for mi, ei, ci in zip(m, energies, colors):
+            ax.hlines(ei, mi - 0.3, mi + 0.3, colors=plt.cm.plasma(ci), linewidth=3)
+
         ax.set_xlabel("m")
         ax.set_ylabel("Zeeman energy (kHz)")
         ax.set_title(f"Zeeman energies of all states in j={j}, B={self.b_field_gauss} G")
-        fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=ax)
+        # fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=ax)
+
+        cbar = fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=ax)
+        cbar.set_label("spin")
+
+        ax.set_xlim(-j-1, j+1)
+        ax.set_xticks([i+0.5 for i in range(-j - 1, j + 1)])
+
 
         # plot the difference between neibouring states on arrows conecting them
         for transition in transitions_in_j.itertuples():
@@ -362,76 +372,53 @@ class CaH:
             energy2 = self.state_df.loc[(self.state_df["j"] == j) & (self.state_df["m"] == m2) & (self.state_df["xi"] == xi2)].iloc[0].zeeman_energy_khz
             energy_diff = transition.energy_diff
             coupling = transition.coupling
-            ax.arrow(float(m1), float(energy1), -1.0, float(energy_diff), head_width=0.1, head_length=0.1, linestyle="dotted", color="black")
+            ax.annotate(
+                "", 
+                xy=(float(m1)-1.0, float(energy1) + float(energy_diff)), 
+                xytext=(float(m1), float(energy1)),
+                arrowprops=dict(arrowstyle='<->', color='gray', lw=1)
+            )
+            # ax.arrow(float(m1), float(energy1), -1.0, float(energy_diff), head_width=0.1, head_length=0.3, linestyle="-", color="gray", length_includes_head=True)
             # add the energy difference as text on the arrow
-            ax.text((m1 + m2) / 2.0 - 0.2, (energy1 + energy2) / 2.0, f"{energy_diff:.3f}", fontsize=10)
+            ax.text((3*m1 + m2) / 4.0 -0.5, (3*energy1 + energy2) / 4.0, f"{energy_diff:.2f} kHz", fontsize=8, color="gray")
             # add the coupling strength as text on the arrow
-            ax.text((m1 + m2) / 2.0, (energy1 + energy2) / 2.0 - 0.9, f"{coupling:.3f}", fontsize=10, color="red")
+            ax.text((3*m1 + m2) / 4.0 -0.5, (3*energy1 + energy2) / 4.0 - 0.9, f"{coupling:.2f}", fontsize=8, color="red")
 
         plt.show()
         plt.close()
 
-    def prova(self, j: int, data_ac: list):
-        """
-        Plot the Zeeman energies of all states in a given j value.
-        """
-        states_in_j = self.state_df.loc[self.state_df["j"] == j]
-        transitions_in_j = self.transition_df[self.transition_df["j"] == j]
-        m = states_in_j["m"].to_numpy()
-        energies = states_in_j["zeeman_energy_khz"].to_numpy()
-        spin_up = states_in_j["spin_up"].to_numpy()
-        spin_down = states_in_j["spin_down"].to_numpy()
-        colors = spin_up**2 - spin_down**2
 
+    def fom(self, b_start: float, b_stop: float, j_start: float, j_stop: float):
 
-        # With AC stark shift
-        rabi_rate_mhz_1 = data_ac[0] # 0.002
-        rabi_rate_mhz_2 = data_ac[1] # 0.004
-        q1 = data_ac[2] # qls.Polarization(1, 0, 0)
-        q2 = data_ac[3] # qls.Polarization(0, 0, 1)
-        ac_stark_shifts = qls.get_ac_stark_shifts(self, rabi_rate_mhz_1, rabi_rate_mhz_2, q1, q2)
-        energies_shifted = energies + ac_stark_shifts[states_in_j.index] * 1e3
+        gj = self.gj
+        cij = self.cij_khz
 
-        diff = energies_shifted - energies
+        num_points = np.abs(b_stop-b_start)*100
+        B_values = np.linspace(b_start, b_stop, num_points) 
+        J_values = np.linspace(j_start, j_stop, num_points) 
 
+        # Crea le griglie di B e J
+        B, J = np.meshgrid(B_values, J_values)
 
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 16))
-        ax1.scatter(m, energies, marker="_", c=colors, cmap="plasma", s=500, linewidths=5)
-        ax1.scatter(m, energies_shifted, marker=".", c=colors, cmap="plasma", s=500, linewidths=5)
-        ax1.set_xlabel("m")
-        ax1.set_ylabel("Zeeman energy (kHz)")
-        ax1.set_title(f"Zeeman energies of all states in j={j}, B={self.b_field_gauss} G")
-        ax1.set_xticks(m)
+        cb = mu_N * B * 1e-4 / h / 1e3
 
-        # plot the difference between neibouring states on arrows conecting them
-        for transition in transitions_in_j.itertuples():
-            m1 = transition.m1
-            xi1 = transition.xi1
-            energy1 = self.state_df.loc[(self.state_df["j"] == j) & (self.state_df["m"] == m1) & (self.state_df["xi"] == xi1)].iloc[0].zeeman_energy_khz
-            m2 = transition.m2
-            xi2 = transition.xi2
-            energy2 = self.state_df.loc[(self.state_df["j"] == j) & (self.state_df["m"] == m2) & (self.state_df["xi"] == xi2)].iloc[0].zeeman_energy_khz
-            energy_diff = transition.energy_diff
-            coupling = transition.coupling
-            ax1.arrow(float(m1), float(energy1), -1.0, float(energy_diff), head_width=0.1, head_length=0.1, linestyle="dotted", color="black")
-            # add the energy difference as text on the arrow
-            ax1.text((m1 + m2) / 2.0 - 0.2, (energy1 + energy2) / 2.0, f"{energy_diff:.3f}", fontsize=10)
-            # add the coupling strength as text on the arrow
-            ax1.text((m1 + m2) / 2.0, (energy1 + energy2) / 2.0 - 0.9, f"{coupling:.3f}", fontsize=10, color="red")
+        x = 1 / 2 * np.sqrt(cij**2 * ((J + 1 / 2) ** 2) + (- cb * (gj - gI)) ** 2)
+        y = - cb * (gj - gI) / 2 
 
+        h0 = 1 - np.abs(y/x)
 
-        ax2.scatter(m, diff, c=colors)
-        ax2.set_xlabel("m")
-        ax2.set_ylabel("Energy difference (kHz)")
-        ax2.set_title(f"Energy difference: comparison WITH and WITHOUT AC Stark shift. j={j}, B={self.b_field_gauss} G")
-        ax2.set_xticks(m)
-        ax2.grid()
+        F = 1 - h0
 
-        fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=[ax1])
-        fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=[ax2])
-
+        plt.figure(figsize=(8, 6))
+        c = plt.contourf(J, B, F, 200, cmap='viridis', vmin=0, vmax=1)  # Imposta il range della colorbar tra 0 e 1
+        plt.colorbar(c, label='F')
+        plt.xlabel('J')
+        plt.ylabel('B')
+        plt.title('Heatmap of F')
+        plt.grid(True)
         plt.show()
-        plt.close()
+        
+
 
     def save_data(self):
         self.state_df.to_csv(f"molecule_data/{self.name}_B[{self.b_field_gauss:.2f}]_Jmax[{self.j_max}]_states.csv", index=False)
@@ -445,7 +432,7 @@ class CaOH(CaH):
     """g factor for J"""
     cij_khz: float = 1.49
     """coupling strength between proton spin and molecule rotation, in kHz"""
-    br_ghz: float = 11.0
+    br_ghz: float = 10.96    
     """rotational constant, in GHz"""
     omega_0_thz: float = 1100.0
     """frequency of the electronic transition between ground state and 1st excited state, in THz"""
@@ -454,160 +441,8 @@ class CaOH(CaH):
     coupling_coefficient: float = 1.
     """coupling coefficient, for now to be able to compare with the NIST value"""
 
-    def __init__(self, b_field_gauss: float, j_max: int) -> None:
-        super().__init__(b_field_gauss, j_max)
+    def __init__(self, b_field_gauss: float, j_max: int, gj_list: list[float] = None, cij_list: list[float] = None) -> None:
+        super().__init__(b_field_gauss, j_max, gj_list, cij_list)
 
 
-class CaOH_dm2(CaH):
-    name: str = "CaOH_dm2"
-    """name of the molecule"""
-    gj: float = -0.036
-    """g factor for J"""
-    cij_khz: float = 1.49
-    """coupling strength between proton spin and molecule rotation, in kHz"""
-    br_ghz: float = 11.0
-    """rotational constant, in GHz"""
-    omega_0_thz: float = 1100.0
-    """frequency of the electronic transition between ground state and 1st excited state, in THz"""
-    omega_thz: float = 280.0
-    """frequency of the Raman beam, in THz"""
-    coupling_coefficient: float = 1.0
-    """coupling coefficient, for now to be able to compare with the NIST value"""
-
-    def __init__(self, b_field_gauss: float, j_max: int) -> None:
-        super().__init__(b_field_gauss, j_max)
-
-    @classmethod
-    def from_file_dm2(cls, b_field_gauss: float, j_max: int):
-        """
-        Load the molecule data from the file. If the file does not exist, calculate the data from the scratch.
-        """
-        new_instance = cls(b_field_gauss, j_max)
-        states_file = Path(f"molecule_data/{cls.name}_B[{b_field_gauss:.2f}]_Jmax[{j_max}]_states.csv")
-        transitions_file = Path(f"molecule_data/{cls.name}_B[{b_field_gauss:.2f}]_Jmax[{j_max}]_transitions.csv")
-        if states_file.exists() and transitions_file.exists():
-            new_instance.state_df = pd.read_csv(states_file)
-            new_instance.transition_df = pd.read_csv(transitions_file)
-        else:
-            new_instance.init_states()
-            new_instance.init_transition_dm2_dataframe()
-            new_instance.save_data()
-        return new_instance
-
-    @classmethod
-    def from_calculation_dm2(cls, b_field_gauss: float, j_max: int):
-        """
-        Calculate the molecule data from the scratch.
-        """
-        new_instance = cls(b_field_gauss, j_max)
-        new_instance.init_states()
-        new_instance.init_transition_dm2_dataframe()
-        new_instance.save_data()
-        return new_instance
-
-    def init_transition_dm2_dataframe(self):
-        """
-        Initialize the transition dataframe.
-        """
-        for j in range(self.j_max + 1):
-            states_in_j = self.state_df.loc[self.state_df["j"] == j]
-            states_index = states_in_j.index.to_numpy()
-            states_array = states_in_j.to_numpy()
-            m_len = 2 * j + 1
-            transition_list = []
-            for index, state1 in enumerate(states_array):
-                index1 = states_index[index]
-                m1, xi1, zeeman_energy_khz1 = state1[1], state1[2], state1[5]
-
-                if index == 0 or index == 1 or index == m_len:
-                    # no transition from the Xi.minus left two edge state
-                    # no transition from the Xi.plus left edge state
-                    continue
-
-                if index == 2 or index == m_len + 1:
-                    # the states right next to the Xi.minus left edge state
-                    index2 = states_index[0]
-                    state2 = states_array[0]
-                    m2, xi2, zeeman_energy_khz2 = state2[1], state2[2], state2[5]
-
-                    energy_diff = zeeman_energy_khz2 - zeeman_energy_khz1
-                    coupling = self.coupling_coefficient * self.get_raman_coupling(index1, index2, -1, -1)
-                    transition_list.append([j, m1, xi1, m2, xi2, index1, index2, energy_diff, coupling])
-                    continue
-
-                index2 = states_index[index - 2]
-                state2 = states_array[index - 2]
-                m2, xi2, zeeman_energy_khz2 = state2[1], state2[2], state2[5]
-                energy_diff = zeeman_energy_khz2 - zeeman_energy_khz1
-                coupling = self.coupling_coefficient * self.get_raman_coupling(index1, index2, -1, -1)
-                transition_list.append([j, m1, xi1, m2, xi2, index1, index2, energy_diff, coupling])
-
-                if xi1:  # Xi.plus
-                    index2 = states_index[index - m_len - 1]
-                    state2 = states_array[index - m_len - 1]
-                    m2, xi2, zeeman_energy_khz2 = state2[1], state2[2], state2[5]
-                    energy_diff = zeeman_energy_khz2 - zeeman_energy_khz1
-                    coupling = self.coupling_coefficient * self.get_raman_coupling(index1, index2, -1, -1)
-                    transition_list.append([j, m1, xi1, m2, xi2, index1, index2, energy_diff, coupling])
-                else:  # Xi.minus
-                    index2 = states_index[index + m_len - 3]
-                    state2 = states_array[index + m_len - 3]
-                    m2, xi2, zeeman_energy_khz2 = state2[1], state2[2], state2[5]
-                    energy_diff = zeeman_energy_khz2 - zeeman_energy_khz1
-                    coupling = self.coupling_coefficient * self.get_raman_coupling(index1, index2, -1, -1)
-                    transition_list.append([j, m1, xi1, m2, xi2, index1, index2, energy_diff, coupling])
-
-            self.transition_df = pd.concat([self.transition_df, pd.DataFrame(transition_list, columns=self.transition_df_columns)], ignore_index=True)
-
-    def plot_zeeman_levels_dm2(self, j: int):
-        """
-        Plot the Zeeman energies of all states in a given j value.
-        """
-        states_in_j = self.state_df.loc[self.state_df["j"] == j]
-        transitions_in_j = self.transition_df[self.transition_df["j"] == j]
-        m = states_in_j["m"].to_numpy()
-        energies = states_in_j["zeeman_energy_khz"].to_numpy()
-        spin_up = states_in_j["spin_up"].to_numpy()
-        spin_down = states_in_j["spin_down"].to_numpy()
-        colors = spin_up**2 - spin_down**2
-
-        fig, ax = plt.subplots(figsize=(12, 8))
-        ax.scatter(m, energies, marker="_", c=colors, cmap="plasma", s=500, linewidths=5)
-        ax.set_xlabel("m")
-        ax.set_ylabel("Zeeman energy (kHz)")
-        ax.set_title(f"Zeeman energies of all states in j={j}, B={self.b_field_gauss} G")
-        fig.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=-1, vmax=1), cmap="plasma"), ax=ax)
-
-        # plot the difference between neibouring states on arrows conecting them
-        for transition in transitions_in_j.itertuples():
-            m1 = transition.m1
-            xi1 = transition.xi1
-            energy1 = self.state_df.loc[(self.state_df["j"] == j) & (self.state_df["m"] == m1) & (self.state_df["xi"] == xi1)].iloc[0].zeeman_energy_khz
-            m2 = transition.m2
-            xi2 = transition.xi2
-            energy2 = self.state_df.loc[(self.state_df["j"] == j) & (self.state_df["m"] == m2) & (self.state_df["xi"] == xi2)].iloc[0].zeeman_energy_khz
-            energy_diff = transition.energy_diff
-            coupling = transition.coupling
-            ax.arrow(float(m1), float(energy1), -2.0, float(energy_diff), head_width=0.1, head_length=0.1, linestyle="dotted", color="black")
-            # add the energy difference as text on the arrow
-            ax.text((m1 + m2) / 2.0 - 0.2, (energy1 + energy2) / 2.0, f"{energy_diff:.3f}", fontsize=10)
-            # add the coupling strength as text on the arrow
-            ax.text((m1 + m2) / 2.0, (energy1 + energy2) / 2.0 - 0.9, f"{coupling:.3f}", fontsize=10, color="red")
-
-        plt.show()
-        plt.close()
-
-
-Molecule: TypeAlias = CaH | CaOH | CaOH_dm2
-
-if __name__ == "__main__":
-    # test = CaH(6.5)
-    # J = 6
-    # test.plot_zeeman_levels(J, include_j_to_j=False)
-    test2 = CaOH(3.1, 30)
-    J = 2
-    # test2.init_transition_dataframe(J)
-    # J = 1
-    # test2.init_transition_dataframe(J)
-    # print(test2.transition_df)
-    # test2.plot_zeeman_levels(J)
+Molecule: TypeAlias = CaH | CaOH 
